5fda03e7c81ebe3ddcfed524a8d708906c21bb02
nirtendra <nirtendrayadav12@gmail.com>
2023-12-29 21:12:15 +1100

    complete lab test for function

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // code your solution here
+
+
+
+
+function saturdayFun( message = "roller-skate"){
+    return `This Saturday, I want to ${message}!`;
+}
+console.log(saturdayFun())  //hoisting function calling before delclaration 
+
+
+function mondayWork(activity= "go to the office"){
+    return `This Monday, I will ${activity}.`
+
+}
+
+function wrapAdjective(initialized = "*") {
+    return function(adjective= "a hard worker") {
+        return `You are ${initialized}${adjective}${initialized}!`;
+    };
+
+    function fn(initialized = "||") {
+        return function(adjective= "a dedicated programmer") {
+        return `You are ${initialized}${adjective}${initialized}!`;
+    }
+   
+    
+    }
+}
+
+
+
+
+
